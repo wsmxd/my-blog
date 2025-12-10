01f839371af17e411e1066d62f3b8f0ed8615622
wsmxd <mxd2166846893@163.com>
2025-12-10 15:56:52 +0800

    改用next.config.js 的 redirects做重定向

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'mxd-blog.vercel.app' }],
+        destination: 'https://wsmxd.top/:path*',
+        permanent: true, // 301 redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
